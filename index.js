7278fec396e1f5a840a75b880e83c0397ecfd79b
Victor Alves <victor.alves@leadforce.com.br>
2022-05-12 21:23:32 -0300

    add timer gameover and paused

--- index.js
+++ index.js
@@ -103,6 +103,39 @@ function retangularCollision({ retangle1, retangle2 }) {
   )
 }
 
+function setWinner({ player, enemy, timerId }) {
+  clearTimeout(timerId)
+  document.querySelector('#displayMessage').style.visibility = 'visible'
+  gameOver = true
+  if (player.health === enemy.health) {
+    document.querySelector('#displayMessage').innerHTML = 'Tie'
+  } else if (player.health > enemy.health) {
+    document.querySelector('#displayMessage').innerHTML = 'Player 1 Wins'
+  } else if (player.health < enemy.health) {
+    document.querySelector('#displayMessage').innerHTML = 'Player 2 Wins'
+  }
+}
+
+let timer = 60
+let timerId
+let gameOver = false
+let paused = false
+
+function decreaseTimer() {  
+  if (!paused) {
+    if (timer > 0) {
+      timerId = setTimeout(decreaseTimer, 1000)
+      timer--
+      document.querySelector('#timer').innerHTML = timer
+    }
+    if (timer === 0) {    
+      setWinner({ player, enemy, timerId })
+    }
+  }
+}
+
+decreaseTimer()
+
 const player = new Sprite({
   position: {
     x: 0,
@@ -128,103 +161,120 @@ const enemy = new Sprite({
 })
 
 function animate() {
-  window.requestAnimationFrame(animate)
-  c.fillStyle = 'black'
-  c.fillRect(0, 0, canvas.width, canvas.height)
-  enemy.update()
-  player.update()  
+  if (!paused) {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    enemy.update()
+    player.update()  
 
-  player.velocity.x = 0
-  if (keys.a.pressed) {
-    player.velocity.x = -5
-  } else if (keys.d.pressed) {
-    player.velocity.x = 5
-  }
+    player.velocity.x = 0
+    if (keys.a.pressed) {
+      player.velocity.x = -5
+    } else if (keys.d.pressed) {
+      player.velocity.x = 5
+    }
 
-  enemy.velocity.x = 0
-  if (keys.ArrowLeft.pressed) {
-    enemy.velocity.x = -5
-  } else if (keys.ArrowRight.pressed ) {
-    enemy.velocity.x = 5
-  }
+    enemy.velocity.x = 0
+    if (keys.ArrowLeft.pressed) {
+      enemy.velocity.x = -5
+    } else if (keys.ArrowRight.pressed ) {
+      enemy.velocity.x = 5
+    }
 
+    //collision
+    if (
+      retangularCollision({
+        retangle1: player,
+        retangle2: enemy
+      }) && player.isAttacking
+    ) {
+      player.isAttacking = false
+      enemy.health -= 10
+      document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+    }
 
-  //collision
-  if (
-    retangularCollision({
-      retangle1: player,
-      retangle2: enemy
-    }) && player.isAttacking
-  ) {
-    player.isAttacking = false
-    enemy.health -= 20
-    document.querySelector('#enemyHealth').style.width = enemy.health + '%'
-  }
+    if (
+      retangularCollision({
+        retangle1: enemy,
+        retangle2: player
+      }) && enemy.isAttacking
+    ) {
+      enemy.isAttacking = false
+      player.health -= 10
+      document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
 
-  if (
-    retangularCollision({
-      retangle1: enemy,
-      retangle2: player
-    }) && enemy.isAttacking
-  ) {
-    enemy.isAttacking = false
-    player.health -= 20
-    document.querySelector('#playerHealth').style.width = player.health + '%'
+    //end game on health
+    if (enemy.health <= 0 || player.health <= 0) {
+      setWinner({ player, enemy, timerId })
+    }
   }
 }
-
-
 
 animate()
 
 window.addEventListener('keydown', (event) => {
-  switch (event.key) {
-    case 'd':
-      keys.d.pressed = true
-    break
-    case 'a':
-      keys.a.pressed = true
-    break
-    case 'w':
-      if (player.jump) {
-        player.velocity.y = -10
-      }
-    break
-    case ' ':
-      player.attack()
-    break
-
-    case 'ArrowRight':
-      keys.ArrowRight.pressed = true
-    break
-    case 'ArrowLeft':
-      keys.ArrowLeft.pressed = true
-    break
-    case 'ArrowUp':
-      if (enemy.jump) {
-        enemy.velocity.y = -10
-      }
-    break
-    case 'ArrowDown':
-      enemy.attack()
-    break
+  if (!gameOver) {
+    switch (event.key) {
+      case 'd':
+        keys.d.pressed = true
+      break
+      case 'a':
+        keys.a.pressed = true
+      break
+      case 'w':
+        if (player.jump) {
+          player.velocity.y = -12
+        }
+      break
+      case ' ':
+        player.attack()
+      break
+  
+      case 'ArrowRight':
+        keys.ArrowRight.pressed = true
+      break
+      case 'ArrowLeft':
+        keys.ArrowLeft.pressed = true
+      break
+      case 'ArrowUp':
+        if (enemy.jump) {
+          enemy.velocity.y = -12
+        }
+      break
+      case 'ArrowDown':
+        enemy.attack()
+      break
+      case 'Escape':
+        if (paused) {
+          paused = false
+          animate()
+          decreaseTimer()
+        }else {
+          paused = true
+        }
+      break
+    }
   }
 })
 
 window.addEventListener('keyup', (event) => {
-  switch (event.key) {
-    case 'd':
-      keys.d.pressed = false
-    break
-    case 'a':
-      keys.a.pressed = false
-    break
-
-    case 'ArrowRight':
-      keys.ArrowRight.pressed = false
-    break
-    case 'ArrowLeft':
-      keys.ArrowLeft.pressed = false
-    break
+  if (!gameOver) {
+    switch (event.key) {
+      case 'd':
+        keys.d.pressed = false
+      break
+      case 'a':
+        keys.a.pressed = false
+      break
+  
+      case 'ArrowRight':
+        keys.ArrowRight.pressed = false
+      break
+      case 'ArrowLeft':
+        keys.ArrowLeft.pressed = false
+      break
+    }
   }
 })
